49ac7334eeb329673a669e38ed664365ebf9bb93
Dharaneesh C <cdharaneesh05@gmail.com>
2026-06-24 10:18:34 +0530

    Time: 0 ms (100%), Space: 19.1 MB (85.98%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        s = str(x)
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        s =str(x)
         return s==s[::-1]
-
         
